f655a123c3f43bbb9823bbcd900db5eb3a0f1aa8
Musab Hassan <musabhassan1999@gmail.com>
2023-03-14 22:56:38 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// ---------Stickey Nav-bar --------------
+window.onscroll = function() {myFunction()};
+    
+var navbar = document.getElementById("my-navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+// ----------------------Swiper code-------------------------
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    delay: 1000,
+    reverseDirection: true,
+    pauseOnMouseEnter: true,
+  },
+  speed: 300,
+  slidesPerView: 3,
+  spaceBetween: 30,
+  grabCursor: true,
+
+  // add break points
+  breakpoints:{
+    0:{
+        slidesPerView:1,
+    },
+    600:{
+        slidesPerView: 2,
+    },
+    900:{
+        slidesPerView: 3,
+    },
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
